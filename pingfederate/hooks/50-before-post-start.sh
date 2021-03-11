9e080f515fe954e88193098908df48dc936f7e17
Chris Price <cprice@pingidentity.com>
2021-03-11 08:30:37 -0800

    Removing P14C bundled IK

--- pingfederate/hooks/50-before-post-start.sh
+++ pingfederate/hooks/50-before-post-start.sh
@@ -36,6 +36,8 @@ echo PingID IK
 rm -f /opt/out/instance/server/default/deploy/pf-pingid-idp-adapter-2.6.jar
 rm -f /opt/out/instance/server/default/deploy/pf-pingid-quickconnection-1.0.1.jar
 rm -f /opt/out/instance/server/default/deploy/PingIDRadiusPCV-2.5.0.jar
+echo P14C IKs
+rm -f /opt/out/instance/server/default/deploy/pf-p14c-*
 # echo "##########
 # "
 # cat /opt/out/instance/server/default/conf/META-INF/hivemodule.xml
